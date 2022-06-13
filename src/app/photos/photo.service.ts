acca514448170d08139c60b45e8792cb298a8431
Steven Hymowitz <shymowitz@cmgfi.com>
2022-06-13 11:28:59 -0700

    formmatting

--- src/app/photos/photo.service.ts
+++ src/app/photos/photo.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse} from '@angular/common/http';
-import { Observable, throwError,of } from 'rxjs';
-import { tap, catchError, scan,filter,reduce, mergeMap, distinct, toArray, map} from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import {
+  tap,
+  catchError,
+  scan,
+  filter,
+  reduce,
+  mergeMap,
+  distinct,
+  toArray,
+  map,
+} from 'rxjs/operators';
 import { IPhoto } from './photo';
-
-
 
 @Injectable({
   providedIn: 'root',
@@ -13,24 +21,13 @@ export class PhotoService {
   photoUrl = 'https://jsonplaceholder.typicode.com/photos';
   constructor(private http: HttpClient) {}
 
-
-    photos$ = this.http.get<IPhoto[]>(this.photoUrl).pipe(
-      tap((data) => console.log(JSON.stringify(data))),
-      catchError(this.handleError)
-    );
-    getImage(id:number) {
-      return this.http.get<IPhoto>(this.photoUrl + '/'+ id);
-    }
-
-    
-    
-  
-    //albums$ = this.photos$.pipe(distinct(a => a.albumId)
-    
-
-    
-    
-  
+  photos$ = this.http.get<IPhoto[]>(this.photoUrl).pipe(
+    tap((data) => console.log(JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+  getImage(id: number) {
+    return this.http.get<IPhoto>(this.photoUrl + '/' + id);
+  }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     let errorMessage: string;
